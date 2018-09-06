7053ef5144ab76e284757f3d8f22169364308a95
Michael Tomae <michael.tomae@gmail.com>
2018-09-06 22:33:24 +0000

    automatically backed up by learn

---
+++ final-project/js/index.js
@@ -0,0 +1,6 @@
+/* event? keydown? on every time
+transition between backgrounds 
+- transitoin when clicking
+- actually change the backgrounds 
+First Initials in the Display slice()?
+change the font type*/
