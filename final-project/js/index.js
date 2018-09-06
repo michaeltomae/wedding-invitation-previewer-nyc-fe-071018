f77dc3dec13c6216271e50abef77cb02ef2e0f3b
Michael Tomae <michael.tomae@gmail.com>
2018-09-06 23:53:51 +0000

    automatically backed up by learn

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -6,7 +6,7 @@ First Initials in the Display slice()?
 change the font type*/
 
 
-input.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', (event) => {
   const lastNameVariable = document.getElementById('last')
   const h2 = document.getElementById('lastName')
   const value = lastNameVariable.value
