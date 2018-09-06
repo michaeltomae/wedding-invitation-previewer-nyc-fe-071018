b9a7b064f015bf26132b45da515533c1ca3ba917
Michael Tomae <michael.tomae@gmail.com>
2018-09-06 23:55:48 +0000

    automatically backed up by learn

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -4,11 +4,11 @@ transition between backgrounds
 - actually change the backgrounds 
 First Initials in the Display slice()?
 change the font type*/
+  const lastNameVariable = document.getElementById('last')
+  const h2 = document.getElementById('lastName')
 
 
 document.addEventListener('keyup', (event) => {
-  const lastNameVariable = document.getElementById('last')
-  const h2 = document.getElementById('lastName')
   const value = lastNameVariable.value
   h2.innerHTML = value
 })
