f1c83c9f705b158c8d8e3b0b45865dfca1c3ce14
Michael Tomae <michael.tomae@gmail.com>
2018-09-07 00:04:35 +0000

    automatically backed up by learn

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -12,6 +12,7 @@ const weddingLocationVariable = document.getElementById('weddingLocationInput')
 
 const h2 = document.getElementById('lastName')
 
+
 document.addEventListener('keyup', (event) => {
   const value = lastNameVariable.value
   h2.innerHTML = value
